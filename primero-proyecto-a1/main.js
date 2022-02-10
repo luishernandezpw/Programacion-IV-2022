66e7de9b84b8ab891a181b6e77f84d9a99584a32
Luis Hernandez <sarco_luis@hotmail.com>
2022-02-10 14:33:48 -0600

    Uso de localstorage

--- primero-proyecto-a1/main.js
+++ primero-proyecto-a1/main.js
@@ -1,7 +1,7 @@
-var db_sistema = openDatabase('dbsistema', '1.0', 'Sistema de Facturacion', 5 * 1024 * 1024);
-if(!db_sistema){
-    alert('Lo siento tu navegado NO soporta BD locales.');
-}
+var idUnicoFecha = ()=>{
+    let fecha = new Date();
+    return Math.floor(fecha.getTime()/1000).toString(16);
+};
 var app = new Vue({
     el: '#appCliente',
     data: {
@@ -20,45 +20,16 @@ var app = new Vue({
     },
     methods: {
         buscarCliente(){
-            /*if( this.buscar.trim().length>0 ){
-                this.clientes = this.clientes.filter(item=>item.nombre.toLowerCase().indexOf(this.buscar.toLowerCase())>=0);
-            } else {
-                this.obtenerClientes();
-            }*/
-            this.obtenerClientes(this.buscar);
+             this.obtenerClientes(this.buscar);
         },
         guardarCliente(){
-            let sql = '',
-                parametros = [];
             if(this.cliente.accion == 'nuevo'){
-                sql = 'INSERT INTO clientes (codigo, nombre, direccion, telefono, dui) VALUES (?,?,?,?,?)';
-                parametros = [this.cliente.codigo,this.cliente.nombre,this.cliente.direccion,this.cliente.telefono,this.cliente.dui];
-            }else if(this.cliente.accion == 'modificar'){
-                sql = 'UPDATE clientes SET codigo=?, nombre=?, direccion=?, telefono=?, dui=? WHERE idCliente=?';
-                parametros = [this.cliente.codigo,this.cliente.nombre,this.cliente.direccion,this.cliente.telefono,this.cliente.dui,this.cliente.idCliente];
-            }else if(this.cliente.accion == 'eliminar'){
-                sql = 'DELETE FROM clientes WHERE idCliente=?';
-                parametros = [this.cliente.idCliente];
+                this.cliente.idCliente = idUnicoFecha();
             }
-            db_sistema.transaction(tx=>{
-                tx.executeSql(sql,
-                    parametros,
-                (tx, results)=>{
-                    this.cliente.msg = 'Cliente procesado con exito';
-                    this.nuevoCliente();
-                    this.obtenerClientes();
-                },
-                (tx, error)=>{
-                    switch(error.code){
-                        case 6:
-                            this.cliente.msg = 'El codigo o el DUI ya existe, por favor digite otro';
-                            break;
-                            
-                        default:
-                            this.cliente.msg = `Error al procesar el cliente: ${error.message}`;
-                    }
-                });
-            });
+            localStorage.setItem(this.cliente.idCliente, JSON.stringify(this.cliente));
+            this.cliente.msg = 'Cliente procesado con exito';
+            this.nuevoCliente();
+            this.obtenerClientes();
         },
         modificarCliente(data){
             this.cliente = data;
@@ -72,15 +43,10 @@ var app = new Vue({
             }
         },
         obtenerClientes(busqueda=''){
-            db_sistema.transaction(tx=>{
-                tx.executeSql(`SELECT * FROM clientes WHERE nombre like "%${busqueda}%" OR codigo like "%${busqueda}%"`, [], (tx, results)=>{
-                    this.clientes = results.rows;
-                    /*this.clientes = [];
-                    for(let i=0; i<results.rows.length; i++){
-                        this.clientes.push(results.rows.item(i));
-                    }*/
-                });
-            });
+            /*this.clientes = [];
+            for(let i=0; i<results.rows.length; i++){
+                this.clientes.push(results.rows.item(i));
+            }*/
         },
         nuevoCliente(){
             this.cliente.accion = 'nuevo';
@@ -95,12 +61,6 @@ var app = new Vue({
         }
     },
     created(){
-        db_sistema.transaction(tx=>{
-            tx.executeSql('CREATE TABLE IF NOT EXISTS clientes(idCliente INTEGER PRIMARY KEY AUTOINCREMENT, '+
-                'codigo char(10) unique, nombre char(75), direccion TEXT, telefono char(10), dui char(10) unique)');
-        }, err=>{
-            console.log('Error al crear la tabla de clientes', err);
-        });
         this.obtenerClientes();
     }
 });
