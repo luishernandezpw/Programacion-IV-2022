913283ba7f405f2478ae8e6e6190c0894e2d9da6
Luis Hernandez <sarco_luis@hotmail.com>
2022-02-14 10:07:44 -0600

    Creacion de componentes en vue.js

--- primero-proyecto-a1/cliente.js
+++ primero-proyecto-a1/cliente.js
@@ -1,16 +1,75 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD Clientes - Offline</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-    <div id="appCliente">
-        <div class="container-fluid">
+Vue.component('cliente', {
+    data:()=>{
+        return {
+            clientes: [],
+            buscar: '',
+            cliente: {
+                accion: 'nuevo',
+                msg : '',
+                idCliente: '',
+                codigo: '',
+                nombre: '',
+                direccion: '',
+                telefono: '',
+                dui: ''
+            }
+        }
+    },
+    methods: {
+        buscarCliente(){
+            this.obtenerDatos(this.buscar);
+        },
+        guardarCliente(){
+            if(this.cliente.accion == 'nuevo'){
+                this.cliente.idCliente = idUnicoFecha();
+            }
+            localStorage.setItem(this.cliente.idCliente, JSON.stringify(this.cliente));
+            this.cliente.msg = 'Cliente procesado con exito';
+            this.nuevoCliente();
+            this.obtenerDatos();
+        },
+        modificarCliente(data){
+            this.cliente = JSON.parse(JSON.stringify(data));
+            this.cliente.accion = 'modificar';
+        },
+        eliminarCliente(data){
+            if( confirm(`¿Esta seguro de eliminar el cliente ${data.nombre}?`) ){
+                this.cliente.idCliente = data.idCliente;
+                this.cliente.accion = 'eliminar';
+                this.guardarCliente();
+            }
+        },
+        obtenerDatos(busqueda=''){
+            this.clientes = [];
+            for(let i=0; i<localStorage.length; i++){
+                let key  = localStorage.key(i),
+                    data = JSON.parse(localStorage.getItem(key));
+                if( this.buscar.length>0 ){
+                    if( data.nombre.toLowerCase().indexOf(this.buscar.toLowerCase())>-1 ){
+                        this.clientes.push(data);
+                    }
+                }else{
+                    this.clientes.push(data);
+                }
+            }
+        },
+        nuevoCliente(){
+            this.cliente.accion = 'nuevo';
+            this.cliente.idCliente = '';
+            this.cliente.codigo = '';
+            this.cliente.nombre = '';
+            this.cliente.direccion = '';
+            this.cliente.telefono = '';
+            this.cliente.dui = '';
+            this.cliente.msg = '';
+            console.log(this.cliente);
+        }
+    }, 
+    created(){
+        this.obtenerDatos();
+    },
+    template: `
+        <div id='appCliente'>
             <form @submit.prevent="guardarCliente" @reset.prevent="nuevoCliente" method="post" id="frmCliente">
                 <div class="card mb-3">
                     <div class="card-header text-white bg-dark">
@@ -103,10 +162,7 @@
                         </tbody>
                     </table>
                 </div>
-            </div> 
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-    <script src="main.js"></script>
-</body>
-</html>
+            </div>
+        </div> 
+    `
+});
--- primero-proyecto-a1/main.js
+++ primero-proyecto-a1/main.js
@@ -3,64 +3,27 @@ var idUnicoFecha = ()=>{
     return Math.floor(fecha.getTime()/1000).toString(16);
 };
 var app = new Vue({
-    el: '#appCliente',
+    el: '#appSistema',
     data: {
-        clientes: [],
-        buscar: '',
-        cliente: {
-            accion: 'nuevo',
-            msg : '',
-            idCliente: '',
-            codigo: '',
-            nombre: '',
-            direccion: '',
-            telefono: '',
-            dui: ''
-        },
+       forms:{
+           'cliente':{ mostrar: false },
+           'categoria':{ mostrar: false },
+           'producto':{ mostrar: false },
+           'proveedor':{ mostrar: false },
+       }
     },
     methods: {
-        buscarCliente(){
-             this.obtenerClientes(this.buscar);
-        },
-        guardarCliente(){
-            if(this.cliente.accion == 'nuevo'){
-                this.cliente.idCliente = idUnicoFecha();
-            }
-            localStorage.setItem(this.cliente.idCliente, JSON.stringify(this.cliente));
-            this.cliente.msg = 'Cliente procesado con exito';
-            this.nuevoCliente();
-            this.obtenerClientes();
-        },
-        modificarCliente(data){
-            this.cliente = data;
-            this.cliente.accion = 'modificar';
-        },
-        eliminarCliente(data){
-            if( confirm(`¿Esta seguro de eliminar el cliente ${data.nombre}?`) ){
-                this.cliente.idCliente = data.idCliente;
-                this.cliente.accion = 'eliminar';
-                this.guardarCliente();
-            }
-        },
-        obtenerClientes(busqueda=''){
-            /*this.clientes = [];
-            for(let i=0; i<results.rows.length; i++){
-                this.clientes.push(results.rows.item(i));
-            }*/
-        },
-        nuevoCliente(){
-            this.cliente.accion = 'nuevo';
-            this.cliente.idCliente = '';
-            this.cliente.codigo = '';
-            this.cliente.nombre = '';
-            this.cliente.direccion = '';
-            this.cliente.telefono = '';
-            this.cliente.dui = '';
-            this.cliente.msg = '';
-            console.log(this.cliente);
-        }
+        
     },
     created(){
-        this.obtenerClientes();
+       
     }
+});
+document.addEventListener('DOMContentLoaded', event=>{
+    let $element = document.querySelectorAll('.mostrar').forEach( (element,index)=>{
+        element.addEventListener('click', e=>{
+            app.forms[e.target.dataset.form].mostrar = true;
+            //app.forms[e.target.dataset.form].obtenerDatos();
+        });
+    });
 });
