5ead9de58ff19ea2ce1b6dbbd8cc80e64fb5646e
Luis Hernandez <sarco_luis@hotmail.com>
2022-05-19 16:50:28 -0600

    Uso de Nodejs + express + mongodb

--- sistema_academico_laravel_a1/server/index.js
+++ sistema_academico_laravel_a1/server/index.js
@@ -1,5 +1,7 @@
 const express = require('express'),
     server = express(),
+    mongodb = require('mongodb').MongoClient,
+    url = 'mongodb://localhost:27017',
     port = 3001;
 server.use(express.json());
 
@@ -7,11 +9,30 @@ server.get('/chat', function(req, resp){
     resp.send('Hola Mundo');
 });
 
-server.get('/usuarios', function(req, resp){
-    console.log(req.body);
-    resp.send('Bienvenido al sitio de chats');
+server.get('/usuarios/list', function(req, resp){
+    mongodb.connect(url, function(err, client){
+        if(err) resp.send(err);
+        const db = client.db('prueba');
+        db.collection('usuarios').find({}).toArray(function(err,data){
+            if(err) resp.send(err);
+            resp.send(data);
+        });
+    });
+});
+server.get('/usuarios/nuevo', function(req, resp){
+    resp.sendFile(__dirname + '/index.html');
+});
+server.post('/usuarios/save', function(req, resp){
+    mongodb.connect(url, function(err, client){
+        if(err) resp.send(err);
+        const db = client.db('prueba');
+        db.collection('usuarios').insertOne(req.body,function(err, result){
+            if(err) resp.send(err);
+            resp.send(result);
+        });
+    });
 });
 
 server.listen(port, function(event){
-    console.log(`Server running on port ${port}`);
+    console.log('Server running on port 3001');
 });
--- sistena_academico_laravel/server/index.js
+++ sistena_academico_laravel/server/index.js
@@ -1,30 +1,33 @@
 const port = 3001;
 
-/*var http = require('http').createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-});
-
-http.listen(port, function() {
-    console.log(`Server running at http://localhost:${port}/`);
-});*/
-
-var http = require('http').Server(),
-    express = require('express'),
+var express = require('express'),
     app = express(),
     mongodb = require('mongodb').MongoClient,
     url = 'mongodb://localhost:27017',
     dbName = 'chatDB';
 
 app.use(express.json());//para poder usar json
-app.get('/chat', function(req, res){
+app.get('/user', function(req, res){
+    res.sendFile(__dirname + '/crud_user.html');
+});
+app.post('/user/save', function(req, resp){
+    let user = req.body;
+    mongodb.connect(url, function(err, client){
+        if(err) resp.send(err);
+        const db = client.db(dbName);
+        db.collection('user').insertOne(user, function(err, result){
+            if(err) resp.send(err);
+            resp.send(result);
+        });
+    });
+});
+app.get('/user/list', function(req, res){
     mongodb.connect(url, function(err, client){
         if( err ) console.log('Error al conectarse a la BD', err);
         const db = client.db(dbName);
         db.collection('user').find({}).toArray(function(er, users){
             res.send(users);
         });
-
     });
 });
 app.get('/juegos', function(req, res){
